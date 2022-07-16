81af23add1131e310df434403a84af00ac2a42f0
Ryan <oocpuxo@gmail.com>
2022-07-16 14:17:51 -0400

    Challenge 16 - Order Log

---
+++ challenge_16.py
@@ -0,0 +1,22 @@
+'''
+You run an e-commerce website and want to record the last N order ids in a log. Implement a data structure to accomplish this, with the following API:
+
+    record(order_id): adds the order_id to the log
+    get_last(i): gets the ith last element from the log. i is guaranteed to be smaller than or equal to N.
+
+You should be as efficient with time and space as possible.
+'''
+
+class OrderLog:
+    def __init__(self, N):
+        self.N = N
+        self.log = []
+
+    def record(self, order_id):
+        self.log.insert(0, order_id)
+        if len(self.log) > self.N:
+            self.log.pop()
+        return
+
+    def get_last(self, i):
+        return self.log(-i)
