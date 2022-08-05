f588805bc3b0507120d82d52e4371765be7085c8
Ryan <oocpuxo@gmail.com>
2022-08-05 13:46:51 -0400

    Challenge 42 - Subset Sum to k

---
+++ challenge_42.py
@@ -0,0 +1,43 @@
+'''
+Given a list of integers S and a target number k, write a function that returns a subset of S that adds up to k. If such a subset cannot be made, then return null.
+
+Integers can appear more than once in the list. You may assume all numbers in the list are positive.
+
+For example, given S = [12, 1, 61, 5, 9, 2] and k = 24, return [12, 9, 2, 1] since it sums up to 24.
+'''
+
+from typing import List
+import copy
+
+def sumSet(S : List[int], k : int):
+    def rSubSet(current_sum, sub_list, rem_list, k):
+        if current_sum == k:
+            return sub_list
+        elif current_sum > k or not rem_list:
+            return None
+        else:
+            for i, num in enumerate(rem_list):
+                sub_list.append(num)
+                result = rSubSet(current_sum + num, sub_list, rem_list[i+1:], k)
+                if result:
+                    return result
+                sub_list.pop()
+        return None
+
+    # Copy and sort input list.
+    sort_S = copy.deepcopy(S)
+    sort_S.sort(reverse=True)
+
+    # Remove any integers greater than k.
+    for i, num in enumerate(sort_S):
+        if num <= k:
+            sort_S = sort_S[i:]
+            break
+    else:
+        # All numbers in the list are greater than k, therefore no subset that sums to k exists.
+        return None
+
+    return rSubSet(0, [], sort_S, k)
+
+print(sumSet([12, 1, 61, 5, 9, 2], 24))
+print(sumSet([12, 1, 61, 5, 9, 2], 25))
