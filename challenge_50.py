ca3e095cc083ad2f7770a92c9f9b04d87db8acca
Ryan <oocpuxo@gmail.com>
2022-08-19 12:16:36 -0400

    Challenge 50 - Arithmetic Tree Minor Comment Update

--- challenge_50.py
+++ challenge_50.py
@@ -1,5 +1,5 @@
 '''
-Suppose an arithmetic expression is given as a binary tree. Each leaf is an integer and each internal node is one of '+', '−', '∗', or '/'.
+Suppose an arithmetic expression is given as a binary tree. Each leaf is an integer and each internal node is one of '+', '-', '*', or '/'.
 
 Given the root to such a tree, write a function to evaluate it.
 
