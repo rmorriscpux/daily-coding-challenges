b5bac152d91ec45b337443b04c036656c68e8d0a
Ryan <oocpuxo@gmail.com>
2022-08-26 19:29:58 -0400

    Challenge 9 - Largest Sum of Non-Adjacent Integers

--- 001-020/challenge_9.py
+++ 001-020/challenge_9.py
@@ -6,10 +6,16 @@ For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1,
 Follow-up: Can you do this in O(N) time and constant space?
 '''
 
-# Will get back to this. I have a partial solution worked out in my head, but it doesn't work on edge cases.
+def largestNonAdjacentSum(int_list):
+    prev_sum = 0
+    sum = 0
 
-def largest_non_adjacent_sum(int_list):
-    # Initialize the sum list.
-    sum_list = [int_list[0]]
+    # Compare in a cascading fashion so that in the end adjacent numbers are not added together.
+    for num in int_list:
+        temp_sum = sum
+        sum = max(sum, prev_sum + num)
+        prev_sum = temp_sum
+    return sum
 
-    
+print(largestNonAdjacentSum([2, 4, 6, 2, 5]))
+print(largestNonAdjacentSum([5, 1, 1, 5]))
