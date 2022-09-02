e3ef254b63c098803851fa5d0663cb2eeb0af2d5
Ryan <oocpuxo@gmail.com>
2022-09-02 03:47:43 -0400

    Challenge 66 - Biased Coin Flip

--- challenge_66.py
+++ challenge_66.py
@@ -4,4 +4,19 @@ Assume you have access to a function toss_biased() which returns 0 or 1 with a p
 Write a function to simulate an unbiased coin toss.
 '''
 
-# https://github.com/rmorriscpux/slot_game
+from random import random
+
+def tossBiased(coin_bias: float):
+    if coin_bias < 0 or coin_bias >= 1:
+        raise ValueError("coin_bias must be between 0 and 1")
+    return 0 if random() <= coin_bias else 1
+
+bias = random()
+trials = 100000
+count = [0, 0]
+for i in range(trials):
+    count[tossBiased(bias)] += 1
+print(f'''Coin Bias: {bias}
+   Trials: {trials}
+ % Zeroes: {count[0] / trials * 100}%
+   % Ones: {count[1] / trials * 100}%''')
