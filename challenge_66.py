322498147a40280101010ee31b370d95c0615c18
Ryan <oocpuxo@gmail.com>
2022-09-02 10:52:45 -0400

    Challenge 66 - Biased Coin Flip Comments Added

--- challenge_66.py
+++ challenge_66.py
@@ -1,16 +1,20 @@
 '''
-Assume you have access to a function toss_biased() which returns 0 or 1 with a probability that's not 50-50 (but also not 0-100 or 100-0). You do not know the bias of the coin.
+Assume you have access to a function toss_biased() which returns 0 or 1 with a probability that's not 50-50 (but also not 0-100 or 100-0).
+You do not know the bias of the coin.
 
 Write a function to simulate an unbiased coin toss.
 '''
 
 from random import random
 
+# Simple function that takes in a threshold between 0 and 1, selects a random number,
+# and returns 0 or 1 depending on whether it's lower or higher than the threshold.
 def tossBiased(coin_bias: float):
     if coin_bias < 0 or coin_bias >= 1:
         raise ValueError("coin_bias must be between 0 and 1")
     return 0 if random() <= coin_bias else 1
 
+# Monte Carlo test of the function.
 bias = random()
 trials = 100000
 count = [0, 0]
