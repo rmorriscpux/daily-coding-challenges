4e4040fab1dddf65ed951c46961c3199ceb96302
Ryan <oocpuxo@gmail.com>
2022-09-02 20:54:40 -0400

    Challenge 45 - rand7() from rand5()

--- challenge_45.py
+++ challenge_45.py
@@ -9,7 +9,14 @@ def rand5():
     return random.randint(1, 5)
 
 def rand7():
-    return ((rand5() + rand5() + rand5() + rand5() + rand5() + rand5() + rand5() - 1) % 7) + 1
+    rand_draw = 25
+    while rand_draw > 21:
+        # Obtain a uniformly random draw between 1 and 25 by using rand5(). Sums one of {0, 5, 10, 15, 20} with one of {1, 2, 3, 4, 5}
+        # To scale, redraw if we get a draw above 21.
+        rand_draw = (rand5() - 1) * 5 + rand5()
+    # Since our range is 21 integers with equivalent occurrence, and 21 is evenly divisible by 7,
+    # then each modulus (0-6) occurs with uniform probability as well. Add 1 to get the range (1-7).
+    return (rand_draw % 7) + 1
 
 results = {}
 for i in range(1, 8):
