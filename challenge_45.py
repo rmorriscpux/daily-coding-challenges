a161323214690f08153907f8e1c408c4afc82d3d
Ryan <oocpuxo@gmail.com>
2022-09-03 07:45:54 -0400

    Challenge 45 - rand7() from rand5() Added comment regarding math behind avg # of iterations.

--- challenge_45.py
+++ challenge_45.py
@@ -13,6 +13,8 @@ def rand7():
     while rand_draw > 21:
         # Obtain a uniformly random draw between 1 and 25 by using rand5(). Sums one of {0, 5, 10, 15, 20} with one of {1, 2, 3, 4, 5}
         # To scale, redraw if we get a draw above 21.
+        # Average number of loop iterations = Σ(4/25)^k for integers k from 0 to ∞, which is approximately 1.1905.
+        # This means the average number of calls to rand5() is 2.3810.
         rand_draw = (rand5() - 1) * 5 + rand5()
     # Since our range is 21 integers with equivalent occurrence, and 21 is evenly divisible by 7,
     # then each modulus (0-6) occurs with uniform probability as well. Add 1 to get the range (1-7).
