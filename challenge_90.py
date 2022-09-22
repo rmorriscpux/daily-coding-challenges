73120c2e5a63a89bd83dd96091391330adcb395c
Ryan <oocpuxo@gmail.com>
2022-09-22 15:27:56 -0400

    Challenge 90 - Exclusionary Random Added object oriented solution.

--- challenge_90.py
+++ challenge_90.py
@@ -21,3 +21,26 @@ def randExcluding(n: int, l: List[int]):
     while rand_value in l:
         rand_value = randrange(0, n)
     return rand_value
+
+# Object Oriented Solution, sacrificing space for accelerated time, reusability.
+class ExclusionaryRandom:
+    def __init__(self, n: int, l: List[int]):
+        if n <= 0:
+            raise ValueError("n must be positive.")
+        self._max = 0
+        self._values = []
+        self.setParameters(n, l)
+
+    def setParameters(self, n: int=-1, l: List[int]=[]):
+        if n > 0:
+            self._max = n
+
+        self._values = []
+        for i in range(0, self._max):
+            if i not in l:
+                self._values.append(i)
+
+        return self
+
+    def getRandomInt(self):
+        return self._values[randrange(0, len(self._values))]
