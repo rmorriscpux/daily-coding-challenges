a297b3e24140a1094117b28493941c3fd2cbb7dd
Ryan <oocpuxo@gmail.com>
2022-09-25 17:06:34 -0400

    Challenge 93 - Largest Binary Search Tree Added Test Cases

--- challenge_93.py
+++ challenge_93.py
@@ -15,7 +15,11 @@ class TreeNode:
         self.left = None
         self.right = None
 
+    def __repr__(self):
+        return f"TreeNode({self.value})"
+
 # Returns the root node and size of the largest binary search tree.
+# If multiple BST sizes are equal, the leftmost BST root node will be returned.
 def getLargestBST(root: TreeNode):
     def rGetLargestBST(node):
         # Determine if the current node has BST properties. Additionally, recur into left and right nodes if they exist.
@@ -41,9 +45,56 @@ def getLargestBST(root: TreeNode):
             size = left_size + right_size + 1
         # When it is not a BST, compare the sizes returned from left and right, and return the largest.
         else:
-            tree_root, size = left_root, left_size if left_size >= right_size else right_root, right_size
+            tree_root, size = (left_root, left_size) if left_size >= right_size else (right_root, right_size)
 
         return tree_root, size, is_bst
 
+    # Only need to retrieve the first two variables from the top level recursion.
     bst_root, bst_size, _ = rGetLargestBST(root)
     return bst_root, bst_size
+
+'''
+      5
+     / \
+    3   6
+   / \   \
+  1   4   8
+ / \     / \
+0   2   7   9
+'''
+
+root = TreeNode(5)
+root.left = TreeNode(3)
+root.left.left = TreeNode(1)
+root.left.left.left = TreeNode(0)
+root.left.left.right = TreeNode(2)
+root.left.right = TreeNode(4)
+root.right = TreeNode(6)
+root.right.right = TreeNode(8)
+root.right.right.left = TreeNode(7)
+root.right.right.right = TreeNode(9)
+
+print(getLargestBST(root))
+
+'''
+      1
+     / \
+    4   6
+   / \   \
+  2   5   8
+ / \     / \
+0   3   7   9
+'''
+
+root2 = TreeNode(1)
+root2.left = TreeNode(4)
+root2.left.left = TreeNode(2)
+root2.left.left.left = TreeNode(0)
+root2.left.left.right = TreeNode(3)
+root2.left.right = TreeNode(5)
+root2.right = TreeNode(6)
+root2.right.right = TreeNode(8)
+root2.right.right.left = TreeNode(7)
+root2.right.right.right = TreeNode(9)
+
+print(getLargestBST(root2))
