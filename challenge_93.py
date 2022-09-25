37b42c7e8b689d48aa2e651e1b4eaa74b6b2093b
Ryan <oocpuxo@gmail.com>
2022-09-25 19:34:04 -0400

    Challenge 93 - Largest Binary Search Tree Fix for case where deep descendant mismatches ancestor criteria.

--- challenge_93.py
+++ challenge_93.py
@@ -5,8 +5,8 @@ Given a tree, return the size of the largest tree/subtree that is a BST.
 '''
 
 # A node is part of a binary search tree (BST) if:
-# - Its left child, if it exists, has a value less than its own value.
-# - Its right child, if it exists, has a value greater than its own value.
+# - Its left descendants, if they exist, have values less than its own value.
+# - Its right descendants, if they exist, have values greater than its own value.
 # - Its children, if any exist, also meet the above criteria.
 
 class TreeNode:
@@ -21,20 +21,23 @@ class TreeNode:
 # Returns the root node and size of the largest binary search tree.
 # If multiple BST sizes are equal, the leftmost BST root node will be returned.
 def getLargestBST(root: TreeNode):
-    def rGetLargestBST(node):
+    def rGetLargestBST(node, left_bound, right_bound):
         # Determine if the current node has BST properties. Additionally, recur into left and right nodes if they exist.
-        # Assume BST until proven otherwise. Leaves with no children count as BSTs.
-        is_bst = True
+        # Assume BST until proven otherwise. Leaves with no children within bounds count as BSTs.
+        is_bst = (node.value > left_bound) and (node.value < right_bound)
         left_root, left_size, left_is_bst = None, 0, True
         right_root, right_size, right_is_bst = None, 0, True
+        new_left_bound, new_right_bound = node.value, node.value
         if node.left:
-            left_root, left_size, left_is_bst = rGetLargestBST(node.left)
             if node.left.value > node.value:
                 is_bst = False
+                new_right_bound = float("inf")
+            left_root, left_size, left_is_bst = rGetLargestBST(node.left, left_bound, new_right_bound)
         if node.right:
-            right_root, right_size, right_is_bst = rGetLargestBST(node.right)
             if node.right.value < node.value:
                 is_bst = False
+                new_left_bound = -float("inf")
+            right_root, right_size, right_is_bst = rGetLargestBST(node.right, new_left_bound, right_bound)
 
         # Both subtrees must be BSTs or empty for this node to be the root of a BST.
         is_bst = is_bst and left_is_bst and right_is_bst
@@ -50,10 +53,11 @@ def getLargestBST(root: TreeNode):
         return tree_root, size, is_bst
 
     # Only need to retrieve the first two variables from the top level recursion.
-    bst_root, bst_size, _ = rGetLargestBST(root)
+    bst_root, bst_size, _ = rGetLargestBST(root, -float("inf"), float("inf"))
     return bst_root, bst_size
 
 '''
+Full tree is a binary search tree.
       5
      / \
     3   6
@@ -77,6 +81,7 @@ root.right.right.right = TreeNode(9)
 print(getLargestBST(root))
 
 '''
+Root is not a binary search tree, but its children are.
       1
      / \
     4   6
@@ -98,3 +103,27 @@ root2.right.right.left = TreeNode(7)
 root2.right.right.right = TreeNode(9)
 
 print(getLargestBST(root2))
+
+'''
+Left branch has a leaf greater than its grandparent node (invalid BST setup on left side).
+      5
+     / \
+    2   6
+   / \   \
+  1   4   8
+ / \     / \
+0   3   7   9
+'''
+
+root3 = TreeNode(5)
+root3.left = TreeNode(2)
+root3.left.left = TreeNode(1)
+root3.left.left.left = TreeNode(0)
+root3.left.left.right = TreeNode(3)
+root3.left.right = TreeNode(4)
+root3.right = TreeNode(6)
+root3.right.right = TreeNode(8)
+root3.right.right.left = TreeNode(7)
+root3.right.right.right = TreeNode(9)
+
+print(getLargestBST(root3))
