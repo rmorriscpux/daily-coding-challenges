553a0a8100e42e0a3615a509a65193614512a18a
Ryan <oocpuxo@gmail.com>
2022-10-02 11:43:30 -0400

    Challenge 100 - Minimum Number of Steps

---
+++ challenge_100.py
@@ -0,0 +1,29 @@
+'''
+You are in an infinite 2D grid where you can move in any of the 8 directions:
+
+ (x,y) to
+    (x+1, y),
+    (x - 1, y),
+    (x, y+1),
+    (x, y-1),
+    (x-1, y-1),
+    (x+1,y+1),
+    (x-1,y+1),
+    (x+1,y-1)
+
+You are given a sequence of points and the order in which you need to cover the points.
+Give the minimum number of steps in which you can achieve it. You start from the first point.
+
+Example: Input: [(0, 0), (1, 1), (1, 2)] Output: 2 It takes 1 step to move from (0, 0) to (1, 1). It takes one more step to move from (1, 1) to (1, 2).
+'''
+
+from typing import List, Tuple
+
+def numSteps(points: List[Tuple[int, int]]):
+    total_steps = 0
+    for i in range(1, len(points)):
+        total_steps += max(abs(points[i][0]-points[i-1][0]), abs(points[i][1]-points[i-1][1]))
+    return total_steps
+
+print(numSteps([(0, 0), (1, 1), (1, 2)]))
+print(numSteps([(0, 0), (1, 1), (-1, -2)]))
