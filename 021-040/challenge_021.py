184c15700b1aafdc88ece09d6ea817840f8d2e1a
Ryan <oocpuxo@gmail.com>
2022-10-13 01:44:28 -0400

    Challenge 21 - Classroom Lectures Improved algorithm with less nested looping.

--- 021-040/challenge_021.py
+++ 021-040/challenge_021.py
@@ -4,32 +4,54 @@ Given an array of time intervals (start, end) for classroom lectures (possibly o
 For example, given [(30, 75), (0, 50), (60, 150)], you should return 2.
 '''
 
-def min_rooms(time_intervals):
-    rooms = []
-    
+from typing import List, Tuple
+
+def minRooms(time_intervals: List[Tuple[int, int]]):
+    time_intervals.sort()
+
+    max_rooms = 0
+    end_queue = []
+
     for lecture_time in time_intervals:
-        lecture_booked = False
-        for room in rooms:
-            room_booked = False
-            # Check the times where the room is booked. Conditions where another room is needed:
-            # 1) Start time or end time is between the booked times.
-            # 2) Start time is before the booked times and end time is after the booked times.
-            for booking in room:
-                if ((lecture_time[0] < booking[1] and lecture_time[0] > booking[0]) or
-                    (lecture_time[1] < booking[1] and lecture_time[1] > booking[0]) or
-                    (lecture_time[0] < booking[0] and lecture_time[1] > booking[1])):
-                    room_booked = True
-                    break
-            # Add booking to room if there is an available slot.
-            if not room_booked:
-                room.append(lecture_time)
-                lecture_booked = True
-        # Add new room if there is no available room.
-        if not lecture_booked:
-            rooms.append([lecture_time])
+        assert lecture_time[0] < lecture_time[1]
 
-    # print(rooms)
+        while len(end_queue) > 0:
+            if end_queue[0] <= lecture_time[0]:
+                end_queue.pop(0)
+            else:
+                break
 
-    return len(rooms)
+        end_queue.append(lecture_time[1])
+        max_rooms = max(max_rooms, len(end_queue))
 
-print(min_rooms([(30,75), (70, 120), (60,150), (0,50)]))
+    return max_rooms
+
+# def min_rooms(time_intervals):
+#     rooms = []
+    
+#     for lecture_time in time_intervals:
+#         lecture_booked = False
+#         for room in rooms:
+#             room_booked = False
+#             # Check the times where the room is booked. Conditions where another room is needed:
+#             # 1) Start time or end time is between the booked times.
+#             # 2) Start time is before the booked times and end time is after the booked times.
+#             for booking in room:
+#                 if ((lecture_time[0] < booking[1] and lecture_time[0] > booking[0]) or
+#                     (lecture_time[1] < booking[1] and lecture_time[1] > booking[0]) or
+#                     (lecture_time[0] < booking[0] and lecture_time[1] > booking[1])):
+#                     room_booked = True
+#                     break
+#             # Add booking to room if there is an available slot.
+#             if not room_booked:
+#                 room.append(lecture_time)
+#                 lecture_booked = True
+#         # Add new room if there is no available room.
+#         if not lecture_booked:
+#             rooms.append([lecture_time])
+
+#     # print(rooms)
+
+#     return len(rooms)
+
+print(minRooms([(30,75), (70, 120), (60,150), (0,50)]))
