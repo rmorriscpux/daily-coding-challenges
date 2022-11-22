4f9006c9e024885d424d47d849b2e62f6c831033
Ryan <oocpuxo@gmail.com>
2022-11-22 17:02:50 -0500

    Challenge 47 fix.

--- 041-060/challenge_047.py
+++ 041-060/challenge_047.py
@@ -7,22 +7,21 @@ For example, given [9, 11, 8, 5, 7, 10], you should return 5, since you could bu
 
 from typing import List
 
-# Brute Force: O(N^2). 0 essentially means never buy.
-# Good luck if you can predict the future.
+# Note: This function as it is here assumes that there is an option to never buy.
 def stockProfit(prices: List[int]):
     if len(prices) <= 1:
         return 0
 
-    buy_price = prices[0]
+    min_price = prices[0]
+    max_price_diff = 0 # Set this to float('-inf') for the case where one must buy and sell once.
 
-    max_profit = 0
+    for price in prices[1:]:
+        if price - min_price > max_price_diff:
+            max_price_diff = price - min_price
 
-    for i in range(1, len(prices)):
-        if prices[i] - buy_price > max_profit:
-            max_profit = prices[i] - buy_price
+        if price < min_price:
+            min_price = price
 
-    recur_max_profit = stockProfit(prices[1:])
-
-    return max_profit if max_profit >= recur_max_profit else recur_max_profit
+    return max_price_diff
 
 print(stockProfit([9, 11, 8, 5, 7, 10]))
