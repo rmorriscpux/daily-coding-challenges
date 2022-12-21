ee43b87563f2b9461f7a895210dd93ce34a7809a
Ryan <oocpuxo@gmail.com>
2022-12-21 13:40:34 -0500

    Challenge 61 - Integer Exponentiation. Divide by zero update.

--- 061-080/challenge_061.py
+++ 061-080/challenge_061.py
@@ -7,11 +7,15 @@ For example, pow(2, 10) should return 1024.
 '''
 
 def pow(x: int, y: int):
-    # Universal Cases: 0^y = 0; x^0 = 1 where x != 0.
+    # Universal Cases: 0^y = 0 where y > 0, 1 where y == 0, error where y < 0. x^0 = 1 where x >=0, -1 where x < 0.
     if x == 0:
+        if y == 0:
+            return 1
+        elif y < 0:
+            raise ZeroDivisionError
         return 0
     if y == 0:
-        return 1
+        return 1 if x > 0 else -1
 
     power = 1
     result = x
