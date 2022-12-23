7ed64a9f84ab96039ce3ced384e863004f476d44
Ryan <oocpuxo@gmail.com>
2022-12-23 18:19:25 -0500

    Challenge 69 - Largest Product of 3 Added quick solution.

--- 061-080/challenge_069.py
+++ 061-080/challenge_069.py
@@ -8,7 +8,7 @@ You can assume the list has at least three integers.
 
 from typing import List
 
-# Works in O(N^3) time and O(1) space. May attempt a quicker solution later.
+# Works in O(N^3) time and O(1) space.
 def largestMultOfThree(int_list: List[int]):
     # NOTE: This function returns None when a list has less than 3 integers. One could also do a check and raise an exception for that case.
     max_product = None
@@ -23,3 +23,25 @@ def largestMultOfThree(int_list: List[int]):
 
 print(largestMultOfThree([-10, -10, 5, 2]))
 print(largestMultOfThree([10, -10, 5, 2]))
+
+# Quicker solution.
+def largestMultOfThreeQuick(int_list: List[int]):
+    if len(int_list) < 3:
+        raise IndexError("Not enough elements in list.")
+    # Assuming O(n*log(n)) sort time.
+    sorted_list = sorted(int_list)
+
+    # Case: All negative.
+    if sorted_list[-1] < 0:
+        return sorted_list[-1] * sorted_list[-2] * sorted_list[-3]
+
+    # Case: Max is 0.
+    elif sorted_list[-1] == 0:
+        return 0 if len(sorted_list) == 3 else sorted_list[-2] * sorted_list[-3] * sorted_list[-4]
+
+    # Case: Positive integers exist.
+    else:
+        return max(sorted_list[-1] * sorted_list[-2] * sorted_list[-3], sorted_list[-1] * sorted_list[0] * sorted_list[1])
+
+print(largestMultOfThreeQuick([-10, -10, 5, 2]))
+print(largestMultOfThreeQuick([10, -10, 5, 2]))
