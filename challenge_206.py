5597093f997f5e6bef195c894ca7147cc9d6fad3
Ryan <oocpuxo@gmail.com>
2023-01-16 12:38:20 -0500

    Challenge 206 - Permutation on Array Added comments.

--- challenge_206.py
+++ challenge_206.py
@@ -8,14 +8,17 @@ given the array ["a", "b", "c"] and the permutation [2, 1, 0], return ["c", "b",
 
 from typing import List
 
+# Helper function to ensure a valid permutation list.
 def validPermutation(P: List[int]):
     return all(map(lambda num: num >= 0 and num < len(P), P)) and len(P) == len(set(P))
 
+# Return a new list with the permutation array applied to the input list.
 def applyPermutation(arr: list, P: List[int]):
     # assert validPermutation(P)
     assert len(arr) == len(P)
     return [arr[P[i]] for i in range(0, len(P))]
 
+# Apply the permutation list to the input list in-place.
 def applyPermutationInPlace(arr: list, P: List[int]):
     # assert validPermutation(P)
     assert len(arr) == len(P)
@@ -24,9 +27,11 @@ def applyPermutationInPlace(arr: list, P: List[int]):
     for cur_index, new_index in enumerate(P):
         cur_val = arr[cur_index]
         if cur_index in stored:
+            # The value originally in the current index was already saved in storage and the index was overwritten. Recover it.
             cur_val = stored[cur_index]
-            del stored[cur_index]
+            del stored[cur_index] # Optional line to improve space management.
 
+        # Store the value currently in the new index if it's still down the line from where cur_index is, since it will be overwritten.
         if new_index > cur_index:
             stored[new_index] = arr[new_index]
 
