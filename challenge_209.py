fe7d46bf747f58eca2fa53a542d318d81b631773
Ryan <oocpuxo@gmail.com>
2023-01-19 13:39:50 -0500

    Challenge 209 - Longest Common Subsequence Added Comments.

--- challenge_209.py
+++ challenge_209.py
@@ -12,6 +12,7 @@ def longestCommonSubseqLen(word1: str, word2: str, word3: str):
         target_letter = target[0]
         substring_set = set()
 
+        # Find the next occurrence of the target letter in comp1 and comp2. If it isn't found, set the index to the length of the comp string.
         for index1, letter1 in enumerate(comp1):
             if letter1 == target_letter:
                 break
@@ -25,10 +26,14 @@ def longestCommonSubseqLen(word1: str, word2: str, word3: str):
             index2 += 1
 
         if index1 == len(comp1) or index2 == len(comp2):
+            # Reached the end of comp1 or comp2, so add the current built_str to the set.
             substring_set.add(built_str)
         else:
+            # When target_letter is found, update the set with the recursion using substrings past
+            # the first instance of target_letter in each string, and add target_letter to built_str.
             substring_set.update(rlongestCommonSubseqLen(target[1:], comp1[index1+1:], comp2[index2+1:], built_str + target_letter))
 
+        # Update the set with recursion using the target string past the first character.
         substring_set.update(rlongestCommonSubseqLen(target[1:], comp1, comp2, built_str))
 
         return substring_set
@@ -37,6 +42,7 @@ def longestCommonSubseqLen(word1: str, word2: str, word3: str):
     substrings.update(rlongestCommonSubseqLen(word2, word3, word1, ""))
     substrings.update(rlongestCommonSubseqLen(word3, word1, word2, ""))
 
+    # Calculate the length of each substring returned in the set and return the max value from that.
     return max(map(lambda s: len(s), substrings))
 
 print(longestCommonSubseqLen("epidemiologist", "refrigeration", "supercalifragilisticexpialodocious"))
