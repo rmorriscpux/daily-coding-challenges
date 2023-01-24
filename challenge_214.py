df8a079960c0d60f129a01a39da1c82cace435b6
Ryan <oocpuxo@gmail.com>
2023-01-24 12:10:05 -0500

    Challenge 214 - Longest Consecutive Run of 1's

---
+++ challenge_214.py
@@ -0,0 +1,20 @@
+'''
+Given an integer n, return the length of the longest consecutive run of 1s in its binary representation.
+
+For example, given 156, you should return 3.
+'''
+
+def longestConsecutiveOnes(n: int):
+    n = abs(n)
+
+    count, max_count = 0, 0
+    while n > 0:
+        if n % 2 == 1:
+            count += 1
+        else:
+            if count > max_count:
+                max_count = count
+            count = 0
+        n >>= 1
+
+    return max_count
