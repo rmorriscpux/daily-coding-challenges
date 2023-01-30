017f8b8478c7be8a8286a77663c29ca1f9d74d08
Ryan <oocpuxo@gmail.com>
2023-01-30 10:19:42 -0500

    Challenge 219 - Connect 4 Added ColumnFullError Exception Added header and column labels to printBoard output. Reworked input handler & playRound for improved clarity & flow of code. Simplified check for full board. Added check for valid max rows and columns at start of game. Added and tweaked comments.

--- challenge_219.py
+++ challenge_219.py
@@ -8,12 +8,17 @@ Design and implement Connect 4.
 
 import os
 
+# Custom exception name to raise when a selected column is full.
+class ColumnFullError(Exception):
+    pass
+
 class Connect4:
     def __init__(self, play: bool=True):
         # Standard Connect 4 game is 7 columns and 6 rows. Modify the below two values here to customize the grid.
+        # These values should be greater than or equal to 4 for the game to work properly. Also, values much higher than standard will cause issues with displaying.
         self._COLS = 7
         self._ROWS = 6
-        # The board is inverted from how matrices are typically represented, for ease of placing discs.
+        # The board is inverted from how matrices are typically represented for ease of placing discs.
         self.board = [['open' for i in range(0, self._ROWS)] for j in range(0, self._COLS)]
         # Symbol map for printing the board.
         self._SYMBOLS = {
@@ -23,16 +28,23 @@ class Connect4:
         }
         # Counters for how many open spaces are left. Quicker than counting open spaces in a column every time an attempt at placing a disc is made.
         self._open_spaces = [self._ROWS for i in range(0, self._COLS)]
-        # Plays automatically when instantiated.
+        # Plays automatically when instantiated by default.
         if play:
             self.start()
 
     # Prints a representation of the current Connect 4 board.
     def printBoard(self):
-        # Clear terminal window. os.system('cls') is used for Windows ('nt') and os.system('clear') is used for Mac/Linux ('posix')
+        # Clear terminal window. 'cls' is used for Windows (os.name == 'nt') and 'clear' is used for Mac/Linux/ChromeOS (os.name == 'posix')
         os.system('cls') if os.name == 'nt' else os.system('clear')
+        # Title
         print("|" + "CONNECT 4".center(self._COLS * 4 - 1) + "|")
+        # Column headers
+        col_label_str = "|"
+        for j in range(1, self._COLS+1):
+            col_label_str += str(j).center(3) + "|"
+        print(col_label_str)
         print("|===" * self._COLS + "|")
+        # Grid
         for i in range(0, self._ROWS):
             row_str = "|"
             for j in range(0, self._COLS):
@@ -40,30 +52,41 @@ class Connect4:
             print(row_str)
             if i < self._ROWS - 1:
                 print("|---" * self._COLS + "|")
+        # Board Bottom
         print("|===" * self._COLS + "|")
         return
 
+    # Play one turn.
+    # Note: Be careful! The board's lists are 0-indexed, but the display and interaction are 1-indexed.
     def playRound(self, color: str):
-        # Subroutine to handle input.
-        def inputHandler(color):
-            print(f"{color}'s Turn.")
-            input_col = input(f"Select Column 1-{self._COLS}: ")
-            while not (input_col.isnumeric() and int(input_col) >= 1 and int(input_col) <= self._COLS):
-                self.printBoard()
-                print("Invalid Input.")
-                print(f"{color}'s Turn.")
-                input_col = input(f"Select Column 1-{self._COLS}: ")
-            return int(input_col) - 1
-
-        assert color in {'Red', 'Black'}
-        self.printBoard()
-        play_col = inputHandler(color)
-        # Check that column is not full.
-        while not self._open_spaces[play_col]:
+        # Subroutine to handle input. Will rerun until an input representing a valid move is entered.
+        def inputHandler(color, err_msg=None):
             self.printBoard()
-            print(f"Column {play_col+1} is full. Select again.")
-            play_col = inputHandler(color)
-        # With valid input, drop disc into corresponding column.
+            if err_msg:
+                print(err_msg)
+            print(f"{color}'s turn.")
+            try:
+                # Input must be resolvable to an integer. ValueError is raised if it is not.
+                play_col = int(input(f"Select Column 1-{self._COLS}: ")) - 1
+                # Integer value must be within the columns on the board.
+                if not (0 <= play_col < self._COLS):
+                    raise ValueError
+                # Cannot place disc if the column is already full.
+                if self._open_spaces[play_col] == 0:
+                    raise ColumnFullError
+
+            except ValueError:
+                play_col = inputHandler(color, f"Invalid Input. Must be an integer between 1 and {self._COLS}")
+
+            except ColumnFullError:
+                play_col = inputHandler(color, f"Column {play_col+1} is full. Select again.")
+
+            return play_col
+
+        # Sanity check.
+        assert color in {'Red', 'Black'}
+        # Get input and drop disc into corresponding column.
+        play_col = inputHandler(color)
         self._open_spaces[play_col] -= 1
         self.board[play_col][self._open_spaces[play_col]] = color
         # Run evaluation.
@@ -71,6 +94,7 @@ class Connect4:
 
         return
 
+    # Routine to evaluate the board for a win after a player's turn.
     def evaluate(self, row: int, col: int):
         # To save time, evaluation only needs to be done starting with the last dropped disc.
         color = self.board[col][row]
@@ -104,7 +128,7 @@ class Connect4:
             if count >= 4:
                 win_flag = True
 
-        # Diagonal \
+        # \ Diagonal
         if not win_flag:
             count = 1
             col_ptr = col - 1
@@ -122,7 +146,7 @@ class Connect4:
             if count >= 4:
                 win_flag = True
 
-        # Diagonal /
+        # / Diagonal
         if not win_flag:
             count = 1
             col_ptr = col - 1
@@ -144,8 +168,8 @@ class Connect4:
             # Four-in-a-row found on the last player's turn. Game ends with that player winning.
             self.printBoard()
             print(f"{color} Wins!")
-        elif all(map(lambda col: col == 0, self._open_spaces)):
-            # Board is completely full. Game is a draw.
+        elif sum(self._open_spaces) == 0:
+            # Board is completely full. Game ends in a draw.
             self.printBoard()
             print("Draw!")
         else:
@@ -159,6 +183,8 @@ class Connect4:
     def start(self):
         self.board = [['open' for i in range(0, self._ROWS)] for j in range(0, self._COLS)]
         self._open_spaces = [self._ROWS for i in range(0, self._COLS)]
+        if sum(self._open_spaces) <= 0:
+            raise ValueError("_ROWS and/or _COLS values are invalid. Check Connect4 class definition.")
         self.playRound("Red")
         return
 
