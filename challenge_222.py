3e0b81024450d1a05943ce5ff0fb49343c571f85
Ryan <oocpuxo@gmail.com>
2023-02-02 17:21:23 -0500

    Challenge 222 - Standardized Path Name

--- challenge_222.py
+++ challenge_222.py
@@ -7,17 +7,26 @@ For example, given "/usr/bin/../bin/./scripts/../", return "/usr/bin/".
 # Note: A properly-formed absolute pathname in Unix-like systems always begins with '/' (root).
 
 def standardizePath(pathname: str):
-    out_path = []
+    out_path_list = []
 
     for name in pathname.split('/'):
-        if name == '..':
-            if len(out_path) > 1:
-                out_path.pop()
-        elif name == '.':
+        # '.' means "Current Directory". Do nothing.
+        if name == '.':
             continue
+        # '..' means "Parent Directory". Remove last from directory list.
+        elif name == '..':
+            # '..' at the root level should be ignored, i.e. only remove the last item in out_path_list if it's past the root.
+            if len(out_path_list) > 1:
+                out_path_list.pop()
+        # Add to directory list.
         else:
-            out_path.append(name)
+            out_path_list.append(name)
 
-    return '/'.join(out_path)
+    out_path = '/'.join(out_path_list)
+    # Remove duplicate '/' characters.
+    while out_path.find('//') != -1:
+        out_path = out_path.replace('//', '/')
+
+    return out_path
 
 print(standardizePath("/usr/bin/../bin/./scripts/../"))
